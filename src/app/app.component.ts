da1d0afd8514c5c0d8f0e2463af320d658bd8435
JuniorQB <eucomcerteza@gmail.com>
2023-02-25 16:38:22 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+interface teste {
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Minha Primeira Aplicação Angular';
+  
+
 }
